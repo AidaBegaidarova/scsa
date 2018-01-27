e1922b6a0d93058f36d513524c94948652c068a3
teststep <xxx@example.com>
2018-01-28 00:48:21 +0700

    fix errors

--- init.sh
+++ init.sh
@@ -1,5 +1,14 @@
 # скрипт привязки файла настроек к nginx
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
-sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+#sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart
+#sudo /etc/init.d/mysql start
+#sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+#sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/nginx/sites-enabled/gunicorn.conf
+#sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
+#sudo /etc/init.d/nginx restart
+sudo service nginx restart
 sudo service gunicorn restart
+sudo service mysql start
+
