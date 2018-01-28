e9599b619bc9ae4c82dcbd18fc44162e2495bb89
teststep <xxx@example.com>
2018-01-28 04:24:40 +0000

    clean init.sh

--- init.sh
+++ init.sh
@@ -1,14 +1,7 @@
-# скрипт привязки файла настроек к nginx
+# скрипт инициализации 
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
-sudo rm /etc/gunicorn.d/*
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
-#sudo /etc/init.d/mysql start
-#sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-#sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
-#sudo /etc/init.d/nginx restart
-#sudo service nginx restart
-#sudo service gunicorn restart
+sudo service nginx restart
+sudo service gunicorn restart
 #sudo service mysql start
 
