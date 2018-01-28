2f4a2381bfde3f3e740fe3753ff1a387b0580570
teststep <xxx@example.com>
2018-01-28 03:33:48 +0000

    solved problem 2

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
-#def app(environ, start_response):
-#    start_response("200 OK", [("Content-Type", "text/plain")])
-#    data = [(item + '\n').encode() for item in environ['QUERY_STRING'].split('&')]
-#    return data
-def app (environ,start_response):
-    status = '200 OK'
-    headers= [('Content-Type','text/plain')]
-    start_response(status,headers)
-    body="\r\n".join(environ['QUERY_STRING'].split('&'))
-    return [body.encode()]
+def app(environ, start_response):
+    start_response("200 OK", [("Content-Type", "text/plain")])
+    data = [(item + '\n').encode() for item in environ['QUERY_STRING'].split('&')]
+    return data
+#def app (environ,start_response):
+#    status = '200 OK'
+#    headers= [('Content-Type','text/plain')]
+#    start_response(status,headers)
+#    body="\r\n".join(environ['QUERY_STRING'].split('&'))
+#    return [body.encode()]
