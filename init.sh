7c9dff66527762efceb8230af6658458495acdf5
teststep <krasnojarov.krasnojar@yandex.ru>
2018-01-28 18:12:39 +0700

    add command for gunicorn_django.conf to init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 # скрипт инициализации 
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+#sudo ln -sf /home/box/web/etc/gunicorn_django.conf   /etc/gunicorn.d/test_django
 sudo service nginx restart
 sudo service gunicorn restart
 #sudo service mysql start
