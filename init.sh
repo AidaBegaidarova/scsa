a5b21336359e36e39fab93195233cec7a54df786
teststep <xxx@example.com>
2018-01-28 04:06:47 +0000

    gunicorn worked with gunicorn conf

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 # скрипт привязки файла настроек к nginx
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo rm /etc/gunicorn.d/*
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
