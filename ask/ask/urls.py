76bdc2fce2031c571aa55b10fc4959ee6a214a4c
teststep <krasnojarov.krasnojar@yandex.ru>
2018-01-30 10:34:36 +0700

    edit urls

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -18,4 +18,10 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^login/$',  include('qa.urls')),
+    url(r'^signup/$',  include('qa.urls')),
+    url(r'^ask/'$,  include('qa.urls')),
+    url(r'^popular/$',  include('qa.urls')),
+    url(r'^new/$',  include('qa.urls')),
+    url(r'^question/.+/$',  include('qa.urls')),
 ]
