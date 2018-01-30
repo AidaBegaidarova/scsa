fb6162e5e8628c29af37eeaa21d91761691253b0
teststep <krasnojarov.krasnojar@yandex.ru>
2018-01-30 11:01:58 +0700

    fix 3

--- init.sh
+++ init.sh
@@ -7,7 +7,7 @@ sudo service nginx restart
 sudo service gunicorn stop
 
 sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:app
-sudo gunicorn -c /home/box/web/etc/gunicorn_django.conf ask/ask/wsgi:application
+sudo gunicorn -c /home/box/web/etc/gunicorn_django.conf ask.ask.wsgi:application
 #sudo service gunicorn restart
 #sudo service mysql start
 
