e8df51dc47b13c6bd65c5f00c8fe78feb20f4456
teststep <krasnojarov.krasnojar@yandex.ru>
2018-01-30 10:52:31 +0700

    change commands for gunucorn service

--- init.sh
+++ init.sh
@@ -1,8 +1,13 @@
 # скрипт инициализации 
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+#sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo ln -sf /home/box/web/etc/gunicorn_django.conf   /etc/gunicorn.d/test_django
+
 sudo service nginx restart
-sudo service gunicorn restart
+sudo service gunicorn stop
+
+sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:app
+sudo gunicorn -c /home/box/web/etc/gunicorn_django.conf ask.wsgi:application
+#sudo service gunicorn restart
 #sudo service mysql start
 
